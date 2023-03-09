f335cf02acb2c003502af4668d33b496222321c4
jakob.aio <103487648+7x7ist14@users.noreply.github.com>
2023-03-09 19:40:25 +0100

    updated config.py
    added command change option

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ TOKEN = ''
 
 #Input the name of the discord channel here (channel not server!):
 CHANNEL_NAME = ''
+
+#if you want to change the command you can do it here:
+COMMAND_PREFIX = '$h'
+# $h is the standart command but you can change it how you want to. for example: %payout
